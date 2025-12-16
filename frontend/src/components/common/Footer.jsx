94a1f15ef50dca17f36ad4fdc5cdc32e3d65e806
Noamakkusu <noahcgo@gmail.com>
2025-12-15 20:00:05 -0500

    Header and footer setup

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,12 @@
+//Footer component will add accessibility to resources later on
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Care Compass. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
