fd2255f3188ec01b1a95fb42c9b7c3ed16b0c2af
JoshuaSubray <subrayjoshua@gmail.com>
2025-12-15 22:08:16 -0500

    Removed merge accident.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -22,23 +22,3 @@ export default function HomePage() {
     </div>
   );
 }
-import { Link } from "react-router-dom";
-import "./HomePage.css";
-
-export default function HomePage() {
-  return (
-    <div className="home-container">
-      <div className="home-inner">
-        <div className="chat-link-wrapper">
-          <Link to="/chat" className="chat-link">
-            Open Chatbot
-          </Link>
-        </div>
-
-        <section className="resources">
-          <h2>Resources</h2>
-        </section>
-      </div>
-    </div>
-  );
-}
