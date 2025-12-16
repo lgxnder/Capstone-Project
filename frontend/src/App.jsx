d343305345fa258fa2880fa5cb709cb17bfdfab0
lgxnder <n01627582@humber.ca>
2025-12-15 20:13:43 -0500

    merged changes from proto-3 branches

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/chat" element={<ChatPage />} />
         </Routes>
       </BrowserRouter>
